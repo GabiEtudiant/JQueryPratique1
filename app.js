6865618521808739213e29ec554b895402d2a563
GabiEtudiant <123960869+GabiEtudiant@users.noreply.github.com>
2023-06-22 11:41:46 -0400

    initiation a JQuery

---
+++ app.js
@@ -0,0 +1,27 @@
+//code pour manipuler le DOM
+// syntaxe jquery $(localisateur).action()
+//on localise element dans le DOM puis on effectue une action sur lui
+// $("p").hide();
+$(document).ready(function()
+{
+    //lorsque je clic les elements se masquent
+    //jquery utilise les tags css pour localiser les elements
+
+    //localisateur par tagname, balise de l'element a manipuler
+    // $("button").click(function(){
+    // $("p").hide();
+    // $("h1").hide();
+    // })
+
+    //type de localilation par id
+    // $("button").click(function(){
+    // $("#demo").hide();
+    // })
+
+    //type de localisation par classe
+    // $("button").click(function(){
+    //     $(".c").hide();
+    // })
+
+    
+});
